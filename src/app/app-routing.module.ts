887e1af834826f5a3ae327db678512043f1a2d42
Vitalii Nikolaienko <vitalii.nikolaienko@student.tuke.sk>
2019-09-30 10:11:55 +0200

    Added 'moment' and 'moments'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { IndexComponent } from './components/pages/index/index.component';
+import { AnimeComponent } from './components/pages/anime/anime.component';
+import { GamesComponent } from './components/pages/games/games.component';
+import { SerialsComponent } from './components/pages/serials/serials.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: IndexComponent},
+    {path: 'anime', component: AnimeComponent},
+    {path: 'games', component: GamesComponent},
+    {path: 'serials', component: SerialsComponent},
+    {path: '404', component: NotFoundComponent},
+    {path: '**', redirectTo: '/404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+import { AnimeComponent } from './components/pages/anime/anime.component';
+import { GamesComponent } from './components/pages/games/games.component';
+import { SerialsComponent } from './components/pages/serials/serials.component';
+import { IndexComponent } from './components/pages/index/index.component';
+import { MomentComponent } from './components/layouts/moment/moment.component';
+import { MomentsComponent } from './components/layouts/moments/moments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    AnimeComponent,
+    GamesComponent,
+    SerialsComponent,
+    IndexComponent,
+    MomentComponent,
+    MomentsComponent
   ],
   imports: [
     BrowserModule,
